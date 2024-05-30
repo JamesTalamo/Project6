5e801543674e9ea3e7af4eead5b55f3559bd2c34
james <jamestalamo4@gmail.com>
2024-05-30 22:50:16 +0100

    update 30/05/2024

--- Backend/Model/Users.js
+++ Backend/Model/Users.js
@@ -2,18 +2,22 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-    username : {
-        type : String,
-        required : true
+    username: {
+        type: String,
+        required: true
     },
-    password : {
-        type : String,
-        required : true
+    password: {
+        type: String,
+        required: true
     },
-    cookieId : {
-        type : String,
-        default : ""
-    }   
+    cookieId: {
+        type: String,
+        default: ""
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 })
 
 module.exports = mongoose.model('users', userSchema)
--- Frontend/routesJS/dashboard/dashboard.js
+++ Frontend/routesJS/dashboard/dashboard.js
@@ -24,87 +24,10 @@ let fetchData = async () => {
 }
 fetchData()
 
-// setTimeout(() => {
-//     // console.log(userInfo)
-//     // let infoName = document.querySelector('#infoName')
-//     // infoName.innerText = userInfo.success.username
-
-
-
-// }, 1000)
-
-
-//BUTTONS
-
-//MAIN DASHBOARD
-let home = document.querySelector('#home')
-home.addEventListener('click', (e) => {
-    e.preventDefault()
-
-    let mainTemplate = document.getElementById('main-template')
-
-    if(mainTemplate.childElementCount !== 0){
-
-        mainTemplate.removeChild(mainTemplate.firstElementChild)
-
-    }
-
-
-    let templateHome = document.createElement('div')
-    templateHome.id = 'homeDashboard'
-    templateHome.classList.add('btn-main-template')
-
-    mainTemplate.appendChild(templateHome)
-
-})
-
-//REGISTERED USER
-let registerUser = document.querySelector('#registerUser')
-registerUser.addEventListener('click', (e) => {
-    e.preventDefault()
-
-    let mainTemplate = document.getElementById('main-template')
-
-
-    if(mainTemplate.childElementCount !== 0){
-
-        mainTemplate.removeChild(mainTemplate.firstElementChild)
-
-
-    }
-
-    let templateRegUser = document.createElement('div')
-    templateRegUser.id = 'registerUserDashboard'
-    templateRegUser.classList.add('btn-main-template')
-
-    mainTemplate.appendChild(templateRegUser)
-
-
-
-})
-
-
-//INFO
-let info = document.querySelector('#info')
-info.addEventListener('click', (e) => {
-    e.preventDefault()
-
-    let mainTemplate = document.getElementById('main-template')
-
-
-    if(mainTemplate.childElementCount !== 0){
-
-        mainTemplate.removeChild(mainTemplate.firstElementChild)
-    }
-
-    let templateInfo = document.createElement('div')
-    templateInfo.id = 'infoDashboard'
-    templateInfo.classList.add('btn-main-template')
-
-    mainTemplate.appendChild(templateInfo)
-
-})
-
+setTimeout(() => {
+    console.log(userInfo)
+    console.log(userInfo.success.createdAt.split('T')[0])
+}, 1000)
 
 
 //Logout 
@@ -138,3 +61,138 @@ logout.addEventListener('click', (e) => {
 
     fetchLogout();
 });
+
+
+
+//BUTTONS !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+//MAIN DASHBOARD
+let home = document.querySelector('#home')
+home.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    let mainTemplate = document.getElementById('main-template')
+
+    if (mainTemplate.childElementCount !== 0) {
+
+        mainTemplate.removeChild(mainTemplate.firstElementChild)
+
+    }
+
+
+    let templateHome = document.createElement('div')
+    templateHome.id = 'homeDashboard'
+    templateHome.classList.add('btn-main-template')
+
+    mainTemplate.appendChild(templateHome)
+
+})
+
+//REGISTERED USER
+let registerUser = document.querySelector('#registerUser')
+registerUser.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    let mainTemplate = document.getElementById('main-template')
+
+
+    if (mainTemplate.childElementCount !== 0) {
+
+        mainTemplate.removeChild(mainTemplate.firstElementChild)
+
+
+    }
+
+    let templateRegUser = document.createElement('div')
+    templateRegUser.id = 'registerUserDashboard'
+    templateRegUser.classList.add('btn-main-template')
+
+
+
+
+
+
+
+    mainTemplate.appendChild(templateRegUser)
+})
+
+
+//INFO
+let info = document.querySelector('#info')
+info.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    let mainTemplate = document.getElementById('main-template')
+
+
+    if (mainTemplate.childElementCount !== 0) {
+
+        mainTemplate.removeChild(mainTemplate.firstElementChild)
+    }
+    //ITO yung sa template nung info!
+    let templateInfo = document.createElement('div')
+    templateInfo.id = 'infoDashboard'
+    templateInfo.classList.add('btn-main-template')
+
+    //Ito yung code para sa name dun sa pinakataas
+    {
+        let customer = document.createElement('div')
+        customer.classList.add('customerText')
+        customer.innerText = 'User / '
+
+        let customerText = document.createElement('div')
+        customerText.innerText = userInfo.success.username
+        customerText.classList.add('customerTextName')
+        customerText.style.display = 'inline'
+
+        customer.appendChild(customerText)
+        templateInfo.appendChild(customer)
+    }
+
+    // ito yung container nung 3 box
+    {
+        let boxContainer = document.createElement('div')
+        boxContainer.style.width = '100%'
+        boxContainer.style.height = '30%'
+        boxContainer.style.display = 'flex'
+        boxContainer.style.alignItems = 'center'
+        boxContainer.style.justifyContent = 'space-around'
+        boxContainer.style.backgroundColor = 'red'
+
+        // para sa date created area ko
+        let box1 = document.createElement('div')
+        box1.classList.add('box')
+        let text1 = document.createElement('text')
+        text1.innerHTML='Date Created'
+        text1.classList.add('box-text-grey')
+        box1.appendChild(text1)
+        let text1Val = document.createElement('text')
+        text1Val.innerText=userInfo.success.createdAt.split('T')[0]
+        text1Val.classList.add('box-text-value')
+        box1.appendChild(text1Val)
+
+
+
+        let box2 = document.createElement('div')
+        box2.classList.add('box')
+
+        let box3 = document.createElement('div')
+        box3.classList.add('box')
+
+        boxContainer.appendChild(box1)
+        boxContainer.appendChild(box2)
+        boxContainer.appendChild(box3)
+
+        templateInfo.appendChild(boxContainer)
+    }
+
+
+
+    //wag buburahin to, ito nagkakabit ng lahat ng code dun sa main template
+    mainTemplate.appendChild(templateInfo)
+
+})
+
+
+
+
